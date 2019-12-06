e354b3c7feaf35f6dac6f85ef7d9e5dfd09939a0
carlosescom <carlos.escom.ipn@gmail.com>
2019-12-05 19:47:03 -0600

    mod: -contractData +contractForm
    now components to read our contracts

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -21,19 +21,6 @@ export default ({ accounts }) => (
     </div>
 
     <div className="section">
-      <h2>SimpleStorage</h2>
-      <p>
-        This shows a simple ContractData component with no arguments, along with
-        a form to set its value.
-      </p>
-      <p>
-        <strong>Stored Value: </strong>
-        <ContractData contract="SimpleStorage" method="storedData" />
-      </p>
-      <ContractForm contract="SimpleStorage" method="set" />
-    </div>
-
-    <div className="section">
       <h2>TutorialToken</h2>
       <p>
         Here we have a form with custom, friendly labels. Also note the token
@@ -41,47 +28,36 @@ export default ({ accounts }) => (
         the <code>hideIndicator</code> prop because we know this variable is
         constant.
       </p>
-      <p>
-        <strong>Total Supply: </strong>
-        <ContractData
-          contract="TutorialToken"
-          method="totalSupply"
-          methodArgs={[{ from: accounts[0] }]}
-        />{" "}
-        <ContractData contract="TutorialToken" method="symbol" hideIndicator />
-      </p>
-      <p>
-        <strong>My Balance: </strong>
-        <ContractData
-          contract="TutorialToken"
-          method="balanceOf"
-          methodArgs={[accounts[0]]}
-        />
-      </p>
-      <h3>Send Tokens</h3>
+      <h3>Total Supply: </h3>
       <ContractForm
-        contract="TutorialToken"
-        method="transfer"
-        labels={["To Address", "Amount to Send"]}
-      />
+        contract="TeebNFT"
+        method="updateMedfileNFT" />
+      <h3>NFTDescription: </h3>
+      <ContractForm
+        contract="TeebNFT"
+        method="NFTDescription" />
+      <h3>grantAccessTo: </h3>
+      <ContractForm
+        contract="TeebNFT"
+        method="grantAccessTo"
+        labels={["To Doctor", "Amount to Send"]} />
+      <h3>revokeAccessTo: </h3>
+      <ContractForm
+        contract="TeebNFT"
+        method="revokeAccessTo"
+        labels={["To Doctor", "Amount to Send"]} />
     </div>
     <div className="section">
-      <h2>ComplexStorage</h2>
+      <h2>Teeb</h2>
       <p>
         Finally this contract shows data types with additional considerations.
         Note in the code the strings below are converted from bytes to UTF-8
         strings and the device data struct is iterated as a list.
       </p>
       <p>
-        <strong>String 1: </strong>
-        <ContractData contract="ComplexStorage" method="string1" toUtf8 />
+        <strong>Total of medfiles recorded: </strong>
+        <ContractData contract="TeebNFT" method="total_mf" />
       </p>
-      <p>
-        <strong>String 2: </strong>
-        <ContractData contract="ComplexStorage" method="string2" toUtf8 />
-      </p>
-      <strong>Single Device Data: </strong>
-      <ContractData contract="ComplexStorage" method="singleDD" />
     </div>
   </div>
 );
