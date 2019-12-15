aae75c067500190d1b9a1a4a822aca2df82e46c1
carlosescom <carlos.escom.ipn@gmail.com>
2019-12-14 20:34:25 -0600

    -+ host: "127.0.0.1",

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,7 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
     develop: {
+      host: "127.0.0.1",
       port: 9545
     },
     ganache: {
